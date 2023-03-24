fd8e1349014cf9599620f3fdcfc16f02dc79508c
ssulphurr <114026438+ssulphurr@users.noreply.github.com>
2023-03-24 22:59:24 +0000

    Deploying to gh-pages from @ ssulphurr/goit-js-hw-11@69026640956e0d5298c9c66095055a84daddbf3e 🚀

---
+++ index.12ab3a25.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("#search-form");e.addEventListener("submit",(function(n){n.preventDefault(),console.log(e.elements.searchQuery.value)}))}();
+//# sourceMappingURL=index.12ab3a25.js.map
---
+++ index.6402d7cb.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("#search-form");e.addEventListener("submit",(function(t){t.preventDefault(),console.log(e.elements.searchQuery.value)}));
+//# sourceMappingURL=index.6402d7cb.js.map
